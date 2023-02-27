f609b9bec2ea1cf71f1d23d92d55fc7c691f8eaf
Alex Henkemeier <amhenkemeier@gmail.com>
2023-02-27 00:46:47 -0600

    update install script to use symlinks instead of copying

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -160,9 +160,9 @@ _G.packer_plugins = {
     path = "/Users/alexhenkemeier/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
-  melange = {
+  ["melange-nvim"] = {
     loaded = true,
-    path = "/Users/alexhenkemeier/.local/share/nvim/site/pack/packer/start/melange",
+    path = "/Users/alexhenkemeier/.local/share/nvim/site/pack/packer/start/melange-nvim",
     url = "https://github.com/savq/melange-nvim"
   },
   ["nvim-cmp"] = {
@@ -225,6 +225,11 @@ _G.packer_plugins = {
     path = "/Users/alexhenkemeier/.local/share/nvim/site/pack/packer/start/rose-pine",
     url = "https://github.com/rose-pine/neovim"
   },
+  ["spin-hud"] = {
+    loaded = true,
+    path = "/Users/alexhenkemeier/.local/share/nvim/site/pack/packer/start/spin-hud",
+    url = "https://github.com/Shopify/spin-hud"
+  },
   ["telescope.nvim"] = {
     loaded = true,
     path = "/Users/alexhenkemeier/.local/share/nvim/site/pack/packer/start/telescope.nvim",
@@ -249,14 +254,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: vim-startify
-time([[Config for vim-startify]], true)
-try_loadstring("\27LJ\2\n8\0\0\2\0\3\0\0056\0\0\0009\0\1\0)\1\0\0=\1\2\0K\0\1\0\27startify_change_to_dir\6g\bvim\0", "config", "vim-startify")
-time([[Config for vim-startify]], false)
 -- Config for: gitsigns.nvim
 time([[Config for gitsigns.nvim]], true)
 try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rgitsigns\frequire\0", "config", "gitsigns.nvim")
 time([[Config for gitsigns.nvim]], false)
+-- Config for: vim-startify
+time([[Config for vim-startify]], true)
+try_loadstring("\27LJ\2\n8\0\0\2\0\3\0\0056\0\0\0009\0\1\0)\1\0\0=\1\2\0K\0\1\0\27startify_change_to_dir\6g\bvim\0", "config", "vim-startify")
+time([[Config for vim-startify]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
