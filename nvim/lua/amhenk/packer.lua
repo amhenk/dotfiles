2e8adf39e5303aa2f5214352de1cd43a028bb62e
Alex Henkemeier <amhenkemeier@gmail.com>
2023-12-16 15:30:02 -0600

    Updates some things I guess

--- nvim/lua/amhenk/packer.lua
+++ nvim/lua/amhenk/packer.lua
@@ -71,6 +71,7 @@ return require('packer').startup(function(use)
     end
   }
 
+  -- navigation
   use {
     'akinsho/bufferline.nvim',
     tag = "v3.*",
@@ -80,13 +81,12 @@ return require('packer').startup(function(use)
     'nvim-tree/nvim-tree.lua',
     requires = 'nvim-tree/nvim-web-devicons'
   }
-  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use('theprimeagen/harpoon')
+  use('alexghergh/nvim-tmux-navigation')
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
-  use('alexghergh/nvim-tmux-navigation')
   use('braxtons12/blame_line.nvim')
-  use('Shopify/spin-hud')
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
