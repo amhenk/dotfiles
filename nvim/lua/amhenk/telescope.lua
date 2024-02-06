7c46aa54c943fb9b7556a7ed8b0c2bcbecbb0c1a
Alex Henkemeier <alex.henkemeier@shopify.com>
2024-02-06 16:20:47 +0000

    Saaafely tries to load fzf extension for telescope

--- nvim/lua/amhenk/telescope.lua
+++ nvim/lua/amhenk/telescope.lua
@@ -1,2 +1,8 @@
-require('telescope').load_extension('fzf')
+local function load_fzf()
+  require('telescope').load_extension('fzf')
+end
+
+-- Sometimes fzf doesn't work 🤔
+local _ = pcall(load_fzf)
+
 require('telescope').load_extension('luasnip')
