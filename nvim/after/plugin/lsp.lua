8a8e9cf33e4df7cb50a3958c00aee0f222d66d58
Alex Henkemeier <amhenkemeier@gmail.com>
2024-05-15 20:43:56 -0500

    Line number highlighting and ruby_lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -8,7 +8,7 @@ lsp_to_install = {
 	'graphql',
 	'marksman',
 	'rust_analyzer',
-	'solargraph',
+	'ruby_lsp',
 	'sorbet',
 	'lua_ls',
   'pyright',
@@ -27,6 +27,7 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "vrn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", ";ca", function() vim.lsp.buf.code_action() end, opts)
 
     vim.keymap.set("n", "<leader>f", function()
         vim.lsp.buf.format({ async = true })
