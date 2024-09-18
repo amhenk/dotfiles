c633a4cd4286cbad81a9724cb1d7fb2f72fadc5c
Alex Henkemeier <alex.henkemeier@shopify.com>
2024-09-18 14:38:06 -0500

    Fixes some stuff, does some things, we use tokyonight now

--- terminal/wezterm/styles.lua
+++ terminal/wezterm/styles.lua
@@ -8,7 +8,7 @@ function _M.apply_to_config(config)
   -- config.color_scheme = "Rose Pine"
   config.font = wezterm.font_with_fallback({
     -- { family = "MesloLGS NF", font_size = 11.0, },
-    { family = "FiraCode Nerd Font Mono", font_size = 11.0, },
+    { family = "FiraCode Nerd Font Mono", scale = 1.0, },
   })
   -- config.hide_tab_bar_if_only_one_tab = true
   config.use_fancy_tab_bar = false
@@ -16,8 +16,7 @@ function _M.apply_to_config(config)
   config.tab_bar_at_bottom = false
   -- Dim inactive panes
   config.inactive_pane_hsb = {
-    saturation = 0.24,
-    brightness = 0.5,
+    brightness = 0.8,
   }
   config.window_padding = {
     left = "0.5cell",
