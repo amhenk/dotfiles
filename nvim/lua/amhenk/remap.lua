281f9bf1e5f9cc94882ed2a741cfc9e22cf36b1f
Alex Henkemeier <amhenkemeier@gmail.com>
2024-10-19 18:11:21 -0500

    Update diagnostic remaps

--- nvim/lua/amhenk/remap.lua
+++ nvim/lua/amhenk/remap.lua
@@ -70,22 +70,23 @@ vim.api.nvim_set_keymap("n", "<leader>H", ":NvimTreeToggle<CR>", { silent = true
 vim.api.nvim_set_keymap("n", ";H", ":NvimTreeFindFile<CR>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap("n", "<C-H>", ":NvimTreeFocus<CR>", { silent = true, noremap = true })
 
--- Trouble mappings
-vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+---------
+--- Diagnostic mappings
+---------
+vim.keymap.set("n", ";tt", function()
+  vim.diagnostic.open_float()
+end,
   {silent = true, noremap = true}
 )
-vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+-- Next Issue
+vim.keymap.set("n", "<leader>ni", function()
+  vim.diagnostic.goto_next()
+end,
   {silent = true, noremap = true}
 )
-vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
+-- Previous Issue
+vim.keymap.set("n", "<leader>pi", function()
+  vim.diagnostic.goto_prev()
+end,
   {silent = true, noremap = true}
 )
